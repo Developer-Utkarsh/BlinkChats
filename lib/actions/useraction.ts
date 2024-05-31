f52c5c431be5dacbc5a9eaa078ab73e05d537374
Utkarsh-Web-2023 <utkarshweb2023@google.com>
2024-05-31 12:54:27 +0530

    fixing some bugs

--- lib/actions/useraction.ts
+++ lib/actions/useraction.ts
@@ -1,32 +1,33 @@
-"use server"
+"use server";
 
-
-import User from "../modals/userModal"
+import User from "../modals/userModal";
 
 import { connect } from "@/lib/db";
 
-
-export async function createUser(user:any){
-    try{
-await connect()
-const newUser = await User.create(user);
-return JSON.parse(JSON.stringify(newUser))
-    }catch(error){
-console.log(error)
-    }
+export async function createUser(user: any) {
+	try {
+		await connect();
+		const newUser = await User.create(user);
+		return JSON.parse(JSON.stringify(newUser));
+	} catch (error) {
+		console.log(error);
+	}
 }
 export async function updateUser(id: string, user: any) {
-    try {
-        await connect();
-        console.log(id);
-        if (id) {
-            const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
-            console.log(user);
-            console.log("updated");
-            return JSON.parse(JSON.stringify(updatedUser));
-        }
-        console.log("No id found");
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await connect();
+		console.log(id);
+		if (id) {
+			const updatedUser = await User.findOneAndReplace({ id }, user, {
+				new: true,
+			});
+			console.log(user);
+			console.log("Updated User", updatedUser);
+			console.log("updated");
+			return JSON.parse(JSON.stringify(updatedUser));
+		}
+		console.log("No id found");
+	} catch (error) {
+		console.log(error);
+	}
 }
